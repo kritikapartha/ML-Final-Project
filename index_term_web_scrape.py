4d9aa2f17e61f785110b94c44a3c976b15497268
Kritika Parthasarathy <kritikap11@sumac.rlogin>
2023-11-23 17:04:36 -0500

    Index term web scraping for test set

--- index_term_web_scrape.py
+++ index_term_web_scrape.py
@@ -12,11 +12,19 @@ html_content = response.text
 soup = BeautifulSoup(html_content, 'html.parser')
 
 # Find the element containing index terms
-index_terms_element = soup.find('div', class_='rlist organizational-chart')
+index_terms_element = soup.find('ol', class_='rlist organizational-chart')
 
-# Extract the text content of the index terms
-if index_terms_element:
-    index_terms = index_terms_element.get_text()
-    print(index_terms)
-else:
-    print("Index terms not found.")
+def parse_tree(ol_element):
+    tree = {}
+    for li in ol_element.find_all('li', recursive=False):
+        h6 = li.find('h6')
+        if h6:
+            links = li.find_all('a')
+            link_texts = [link.text for link in links]
+            tree[h6.text] = {'Indexes': link_texts}
+    return tree
+
+index_term_tree = parse_tree(index_terms_element)
+
+print("Tree Structure with Indexes:")
+print(index_term_tree)
